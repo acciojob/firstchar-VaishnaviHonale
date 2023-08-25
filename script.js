6b78affa44823880933367f01ef376179f2feb2b
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-25 14:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	if(text=''){
+		return "";
+		
+	}
+	for(let i=0;i<text.length;i++){
+		if(text[i]!==' ')
+			return text[i];
+	}
 }
 
 // Do not change the code below
